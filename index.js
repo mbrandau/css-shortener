e01aa0aa893f18019298ac30be28cc99ef1c1ef5
Maximilian Brandau <mbrandau@online.de>
2018-02-10 17:02:39 +0100

    Add comments Shorten code

--- index.js
+++ index.js
@@ -12,21 +12,19 @@ CssShortener.prototype.getMap = function() {
 CssShortener.prototype.importMap = function(map, override) {
   for (var orig in map) {
     if (this._classNameMap[orig] != null) {
-      if (override === true) this._classNameMap[orig] = map[orig];
-    } else this._classNameMap[orig] = map[orig];
+      if (override === true) this._classNameMap[orig] = map[orig]; // Override mapped class name
+    } else this._classNameMap[orig] = map[orig]; // Import class name
   }
 }
 CssShortener.prototype.stream = function(callback) {
   const t = this;
   return replaceStream(/\.[a-z][a-z0-9-_]*/g, function(match) {
-    var id = '';
-    var orig = match.substr(1);
-    if (t._classNameMap[orig] != null) {
-      id = t._classNameMap[orig];
-    } else {
-      id = t._idGenerator();
-      t._classNameMap[orig] = id;
-    }
+    var id;
+    var orig = match.substr(1); // Remove dot infront of class name
+
+    if (t._classNameMap[orig] != null) id = t._classNameMap[orig]; // Use mapped class name
+    else id = t._classNameMap[orig] = t._idGenerator(); // Generate and map new class name
+
     return '.' + id;
   });
 }
