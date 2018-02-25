639be2c946f503b20124ecabbf2df398e63c1449
Maximilian Brandau <mbrandau@online.de>
2018-02-25 18:26:41 +0100

    Add html command Reformat code

--- bin/cli.js
+++ bin/cli.js
@@ -24,17 +24,40 @@ require('yargs')
       type: 'string'
     });
   }, argv => {
-    const csss = new CssShortener();
-    if(argv['import-map'])csss.importMap(JSON.parse(fs.readFileSync(argv['import-map'])));
+    const cs = new CssShortener();
+    if (argv['import-map']) cs.importMap(JSON.parse(fs.readFileSync(argv['import-map'])));
     var inputStream = argv.input ? fs.createReadStream(argv.input) : process.stdin;
     var outputStream = argv.output ? fs.createWriteStream(argv.output) : process.stdout;
     inputStream
       .on('end', () => {
         if (argv.map) {
-          fs.writeFileSync(argv.map, JSON.stringify(csss.getMap()));
+          fs.writeFileSync(argv.map, JSON.stringify(cs.getMap()));
         }
       })
-      .pipe(csss.cssStream())
+      .pipe(cs.cssStream())
+      .pipe(outputStream);
+  })
+  .command('html', 'Replace css class names in an html file', yargs => {
+    return yargs.option('input', {
+      alias: 'i',
+      describe: 'input file',
+      type: 'string'
+    }).option('output', {
+      alias: 'o',
+      describe: 'output file',
+      type: 'string'
+    }).option('map', {
+      alias: 'm',
+      describe: 'input file for the class name map (JSON)',
+      type: 'string'
+    }).demandOption('map');
+  }, argv => {
+    const cs = new CssShortener();
+    cs.importMap(JSON.parse(fs.readFileSync(argv['map'])));
+    var inputStream = argv.input ? fs.createReadStream(argv.input) : process.stdin;
+    var outputStream = argv.output ? fs.createWriteStream(argv.output) : process.stdout;
+    inputStream
+      .pipe(cs.htmlStream())
       .pipe(outputStream);
   })
   .demandCommand()
