fe5c2ec06f922651c4c23f71261256c3c607c094
Maximilian Brandau <mbrandau@online.de>
2018-07-06 16:49:47 +0200

    Add replaceCss function Rearrange code and remove duplicates

--- index.js
+++ index.js
@@ -11,20 +11,9 @@ const CssShortener = function (options) {
     if (!this._options.hasOwnProperty('trimIgnorePrefix')) this._options.trimIgnorePrefix = true;
     this._idGenerator = new IdGenerator(this._options.alphabet);
     this._classNameMap = {};
-};
-CssShortener.prototype.getMap = function () {
-    return this._classNameMap;
-};
-CssShortener.prototype.importMap = function (map, override) {
-    for (let orig in map) {
-        if (this._classNameMap[orig] != null) {
-            if (override === true) this._classNameMap[orig] = map[orig]; // Override mapped class name
-        } else this._classNameMap[orig] = map[orig]; // Import class name
-    }
-};
-CssShortener.prototype.cssStream = function () {
+
     const t = this;
-    return replaceStream(CLASS_NAME_REGEX, function (match, capturingGroup) {
+    const replaceCss = function (match, capturingGroup) {
         if (!capturingGroup) return match;
         let id,
             orig = capturingGroup.substr(1); // Remove dot infront of class name
@@ -37,11 +26,8 @@ CssShortener.prototype.cssStream = function () {
         else id = t._classNameMap[orig] = t._idGenerator(); // Generate and map new class name
 
         return `.${id}`;
-    });
-};
-CssShortener.prototype.htmlStream = function () {
-    const t = this;
-    return replaceStream(HTML_CLASS_REGEX, function (match, capturingGroup) {
+    };
+    const replaceHtml = function (match, capturingGroup) {
         if (!capturingGroup) return match;
         const classes = capturingGroup.trim().split(' ');
         const classCount = classes.length;
@@ -52,22 +38,31 @@ CssShortener.prototype.htmlStream = function () {
             if (i < classCount - 1) result += ' ';
         }
         return `class="${result}"`;
-    });
-};
-CssShortener.prototype.replaceHtml = function (html) {
-    const t = this;
-    return html.replace(HTML_CLASS_REGEX, (match, capturingGroup) => {
-        if (!capturingGroup) return match;
-        const classes = capturingGroup.trim().split(' ');
-        const classCount = classes.length;
-        let result = '';
-        for (let i = 0; i < classCount; i++) {
-            // Check if class is mapped and add it to the result
-            result += (t._classNameMap[classes[i]] != null ? t._classNameMap[classes[i]] : classes[i]);
-            if (i < classCount - 1) result += ' ';
+    };
+
+    this.getMap = function () {
+        return this._classNameMap;
+    };
+    this.importMap = function (map, override) {
+        for (let orig in map) {
+            if (this._classNameMap[orig] != null) {
+                if (override === true) this._classNameMap[orig] = map[orig]; // Override mapped class name
+            } else this._classNameMap[orig] = map[orig]; // Import class name
         }
-        return `class="${result}"`;
-    });
+    };
+
+    this.cssStream = function () {
+        return replaceStream(CLASS_NAME_REGEX, replaceCss);
+    };
+    this.replaceCss = function (css) {
+        return css.replace(CLASS_NAME_REGEX, replaceCss);
+    };
+    this.htmlStream = function () {
+        return replaceStream(HTML_CLASS_REGEX, replaceHtml);
+    };
+    this.replaceHtml = function (html) {
+        return html.replace(HTML_CLASS_REGEX, replaceHtml);
+    };
 };
 
 module.exports = CssShortener;
